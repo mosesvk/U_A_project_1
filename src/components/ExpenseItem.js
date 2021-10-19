e5c904035b042f64c9ba8abfe60d9e2f8d42cc72
mosesvk <kaumatule93@gmail.com>
2021-10-19 17:26:45 -0600

    update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,9 +7,9 @@ const ExpenseItem = (props) => {
   return (
     <div className='expense-item'>
       <div>{date.toISOString()}</div>
-      <div className='expense-item_description'>
+      <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item_price'>${amount}</div>
+        <div className='expense-item__price'>${amount}</div>
       </div>
     </div>
   )
