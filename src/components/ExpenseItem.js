6c712c7e89f496f6c38476420eb055e0be70d07d
mosesvk <kaumatule93@gmail.com>
2021-10-19 16:11:30 -0600

    update ;

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,14 @@
 
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
 
+  const {title, amount, date} = props
+
+  return (
+    <div>
+
+    </div>
+  )
 }
 
 export default ExpenseItem;
