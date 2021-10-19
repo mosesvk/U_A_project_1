bf2e4f7a7fd135405518824c40d155b80724be40
mosesvk <kaumatule93@gmail.com>
2021-10-19 16:41:51 -0600

    update

--- src/App.js
+++ src/App.js
@@ -34,22 +34,22 @@ function App() {
       <ExpenseItem 
         title={expenses[0].title}
         amount={expenses[0].amount}
-        data={expenses[0].date}
+        date={expenses[0].date}
       /> 
       <ExpenseItem 
         title={expenses[1].title}
         amount={expenses[1].amount}
-        data={expenses[1].date}
+        date={expenses[1].date}
       /> 
       <ExpenseItem 
         title={expenses[2].title}
         amount={expenses[2].amount}
-        data={expenses[2].date}
+        date={expenses[2].date}
       /> 
       <ExpenseItem 
         title={expenses[3].title}
         amount={expenses[3].amount}
-        data={expenses[3].date}
+        date={expenses[3].date}
       /> 
     </div>
   );
