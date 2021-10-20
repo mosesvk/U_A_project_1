6c56a9e3031859098a37779f705776cc505ce131
mosesvk <kaumatule93@gmail.com>
2021-10-19 18:22:01 -0600

    update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,9 @@ import './ExpenseItem.css'
 const ExpenseItem = (props) => {
   const {title, amount, date} = props
 
-  const month = date.toLocalString('en-US', {month: 'long'})
-  const day = date.toLocalString('en-US', {day: '2-digit'})
-  const year = date.getFullYear;
+  const month = date.toLocaleString('en-US', {month: 'long'})
+  const day = date.toLocaleString('en-US', {day: '2-digit'})
+  const year = date.getFullYear();
 
   return (
     <div className='expense-item'>
