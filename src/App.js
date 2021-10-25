66721ce011bc7a20d1101690039b8be3eb8ad306
mosesvk <kaumatule93@gmail.com>
2021-10-25 15:21:25 -0600

    update

--- src/App.js
+++ src/App.js
@@ -29,28 +29,28 @@ const DUMMY_EXPENSES = [
   },
 ];
 
-function App() {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses]
-    })
-  }
-
-  return (
-    <div className="App">
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses}/>
-    </div>
-  );
+      return [expense, ...prevExpenses];
+    });
+  };
 
   // return React.createElement(
-  //   'div', 
+  //   'div',
   //   {},
-  //   React.createElement('h2', {}, 'Lets get started!'),
-  //   React.createElement(Expenses, {items: expenses})
-  // )
-}
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+};
 
 export default App;
