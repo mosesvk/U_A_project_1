71a33c584840f1390b2431261d9b1c92dee76a7b
mosesvk <kaumatule93@gmail.com>
2021-10-24 23:24:05 -0600

    update

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -3,10 +3,10 @@ import './ExpenseDate.scss'
 const ExpenseDate = (props) => {
   const {date} = props
 
-  var start = new Date(date);
-  const month = start.toLocaleString('en-US', {month: 'long'})
-  const day = start.toLocaleString('en-US', {day: '2-digit'})
-  const year = start.getFullYear(date);
+  var _date = new Date(date);
+  const month = _date.toLocaleString('en-US', {month: 'long'})
+  const day = _date.toLocaleString('en-US', {day: '2-digit'})
+  const year = _date.getFullYear(date);
 
   return (
     <div id='expense-date'>
